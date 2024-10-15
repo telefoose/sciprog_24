41854d014812a6ae7fdb92465ad1bdf208b22ff1
Alan Levy Bergman <alanlbergman@gmail.com>
2024-10-15 11:33:50 -0300

    adding practical 5b

--- practical5/practical5a.c
+++ practical5/practical5a.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
-void calculate_next_fibonacci(int *current, int *previous){
+void calculate_next_fibonacci(int *current,int *previous){
     int next = *current+*previous;
     *previous = *current;
     *current = next;
 }
 
-int main() {
+int main(){
     int n;
     
     printf("Enter the value of n: ");
@@ -15,12 +15,12 @@ int main() {
     int previous = 0;
     int current = 1;
     
-    if (n >= 1) printf("%d ", previous);
-    if (n >= 2) printf("%d ", current);
+    if (n >= 1) printf("%d ",previous);
+    if (n >= 2) printf("%d ",current);
 
-    for (int i = 2; i < n; i++) {
-        calculate_next_fibonacci(&current, &previous);
-        printf("%d ", current);
+    for (int i = 2; i < n; i++){
+        calculate_next_fibonacci(&current,&previous);
+        printf("%d ",current);
     }
     
     printf("\n");
